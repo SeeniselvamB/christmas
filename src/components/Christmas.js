0eeeabe082041f1a27a98e309582046bf646084e
SeeniselvamB <bseeniselvam2006@gmail.com>
2025-11-17 14:03:13 +0530

    Updates

--- src/components/Christmas.js
+++ src/components/Christmas.js
@@ -49,7 +49,7 @@ const Christmas = () => {
             </div>
 
             <footer className="footer">
-                <p>Made with ❤️ by Seeniselvam B</p>
+                <p>❤️ Made with by Seeniselvam B ❤️</p>
             </footer>
         </div>
     );
